3d2d856b3ae55a706034eb0c747d9197ab2ea2d3
dianthusL <slawomir.gozdziewski@gmail.com>
2021-03-17 12:59:24 +0100

    add home controller

--- WD.Web/Controllers/HomeController.cs
+++ WD.Web/Controllers/HomeController.cs
@@ -1,31 +1,93 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Threading.Tasks;
-using WD.Web.Models;
+using VD.Data.Models;
+using VD.Data.Services;
+using VD.Web.Models;
+using VD.Web.ViewModels;
 
-namespace WD.Web.Controllers
+namespace VD.Web.Controllers
 {
     public class HomeController : Controller
     {
+        #region Fields
+        private readonly IVDRepository _repository;
         private readonly ILogger<HomeController> _logger;
+        public IConfiguration Configuration { get; }
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        #endregion
+
+        #region Constructors
+        public HomeController(ILogger<HomeController> logger, IVDRepository repository,
+            IConfiguration configuration, SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
         {
             _logger = logger;
+            _repository = repository;
+            Configuration = configuration;
+            _signInManager = signInManager;
+            _userManager = userManager;
         }
+        #endregion
 
+        #region Views
+        [HttpGet]
         public IActionResult Index()
         {
+            var model = new HomeViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(HomeViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
+
+                if (result.Succeeded)
+                {
+                    if (_repository.GetUser(model.Email).IsStudent)
+                    {
+                        Student student = _repository.GetStudent(model.Email);
+                        return RedirectToAction("index", "student", new { id = student.ID });
+                    }
+                    else
+                    {
+                        Teacher teacher = _repository.GetTeacher(model.Email);
+                        return RedirectToAction("index", "teacher", new { id = teacher.ID });
+                    }
+                }
+                ModelState.AddModelError(string.Empty, "Nieudane logowanie");
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpPost]
+        public async Task<IActionResult> Register(HomeViewModel model)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = model.Email, Email = model.Email, PasswordHash = model.Password };
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("index", "home");
+                }
+            }
+            return View(model);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
@@ -33,5 +95,10 @@ namespace WD.Web.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        #endregion
+
+        #region Methods
+
+        #endregion
     }
 }
