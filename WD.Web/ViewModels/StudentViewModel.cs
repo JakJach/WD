ad3f63a767184c7f9541320070b1aeb45134be5d
Alex <aleks.bialas@gmail.com>
2021-03-24 12:44:52 +0100

    model views - Student, Teacher, Home

---
+++ WD.Web/ViewModels/StudentViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using WD.Data.Models;
+
+namespace WD.Web.ViewModels
+{
+    public class StudentViewModel
+    {
+        public Student LoggedInStudent { get; set; }
+        public IEnumerable<Classes> StudentClasses { get; set; }
+        public IEnumerable<Project> StudentProjects { get; set; }
+        public Thesis StudentThesis { get; set; }
+    }
+}
---
+++ WD.Web/ViewModels/TeacherViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using WD.Data.Models;
+using WD.Data.Models.Representation;
+
+namespace WD.Web.ViewModels
+{
+    public class TeacherViewModel
+    {
+        public Teacher LoggedInTeacher { get; set; }
+        public IEnumerable<Classes> TeacherClasses { get; set; }
+        public IEnumerable<Project> TeacherProjects { get; set; }
+        public IEnumerable<Student> TeacherStudents { get; set; }
+        public IEnumerable<Thesis> TeacherPromotingTheses { get; set; }
+        public IEnumerable<Thesis> TeacherReviewingTheses { get; set; }
+        public IEnumerable<StudentFinalNote> StudentFinalNotes { get; set; }
+    }
+}
