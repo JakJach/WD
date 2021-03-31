a2057a48cf7ec188ae5b0c59fa04531dafb0462e
Alex <aleks.bialas@gmail.com>
2021-03-31 14:00:17 +0200

    Teacher - ad final note implementation

---
+++ WD.Web/ViewModels/TeacherAddFinalNoteViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WD.Data.Models;
+
+namespace WD.Web.ViewModels
+{
+    public class TeacherAddFinalNoteViewModel
+    {
+        public string ClassesName { get; set; }
+        [RegularExpression(@"[0,2,3,4,5]")]
+        public int Note { get; set; }
+        public Student Student { get; set; }
+    }
+}
