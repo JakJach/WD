c983f1e4bdeccb0952bd78f3e79f2388fc43dc63
Alex <aleks.bialas@gmail.com>
2021-04-21 13:57:08 +0200

    add and review thesis (model and view)

---
+++ WD.Web/ViewModels/StudentAddThesisViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using WD.Data.Models;
+
+namespace WD.Web.ViewModels
+{
+    public class StudentAddThesisViewModel
+    {
+        public string StudentName { get; set; }
+        public string PromoterName { get; set; }
+        public string RevieverName { get; set; }
+        public Thesis Thesis { get; set; }
+        public IFormFile File { get; set; }
+        public IEnumerable<IFormFile> Attachments { get; set; }
+    }
+}
