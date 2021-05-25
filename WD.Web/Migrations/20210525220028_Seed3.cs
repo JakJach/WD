d39dd5ac11e774d93ffb6ae659063aae5b0431fa
Jakub <jakub.jachow@gmail.com>
2021-05-26 00:01:45 +0200

    Seed3

---
+++ WD.Web/Migrations/20210525220028_Seed3.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WD.Web.Migrations
+{
+    public partial class Seed3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Classes",
+                columns: new[] { "ClassId", "Name", "TeacherUserID" },
+                values: new object[,]
+                {
+                    { 1, "Laboratorium Specjalizacyjne", null },
+                    { 2, "Seminarium Dyplomowe", null }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "UserID", "Email", "Name", "Password", "Surname" },
+                values: new object[,]
+                {
+                    { 4, "rotter@agh.edu.pl", "Paweł", "18182f494c2d6cfdda3e893934b7ea5c", "Rotter" },
+                    { 5, "baranowski@agh.edu.pl", "Jerzy", "25d8eb2adfa874aefccb40e132d95639", "Baranowski" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Projects",
+                columns: new[] { "ProjectId", "ClassId", "CreationDate", "Goal", "IsReviewed", "IsSubmitted", "Note", "Review", "ReviewDate", "Scope", "SubmissionDate", "Title" },
+                values: new object[] { 1, 1, new DateTime(2021, 5, 26, 0, 0, 27, 702, DateTimeKind.Local).AddTicks(3289), "Działająca i przetestowana aplikacja z funkcjonalnościami: dodawania ocen, oddawania projektów, składania pracy dyplomowej", false, false, null, null, null, "Wykonanie aplikacji webowej obsługującej wirtualny dziekanat uczelni wyższej", null, "Wirtualny Dziekanat" });
+
+            migrationBuilder.InsertData(
+                table: "Teachers",
+                columns: new[] { "UserID", "CanBePromoter", "CanBeReviewer", "Title" },
+                values: new object[] { 4, true, true, null });
+
+            migrationBuilder.InsertData(
+                table: "Teachers",
+                columns: new[] { "UserID", "CanBePromoter", "CanBeReviewer", "Title" },
+                values: new object[] { 5, true, true, null });
+
+            migrationBuilder.InsertData(
+                table: "Theses",
+                columns: new[] { "ThesisId", "AcceptationDate", "CreationDate", "Goal", "IsAccepted", "IsReviewed", "IsSubmitted", "IsTaken", "PromoterId", "PromoterNote", "PromoterOpinion", "Review", "ReviewDate", "ReviewerId", "ReviewerNote", "Scope", "StudentId", "StudentQualifications", "SubmissionDate", "TakeDate", "Title" },
+                values: new object[] { 1, null, new DateTime(2021, 5, 26, 0, 0, 27, 705, DateTimeKind.Local).AddTicks(8260), "Wykonanie działającej  i gotowej do użycia wtyczki", false, false, false, true, 4, null, null, null, null, 5, null, "Stowrzenie skryptów uczenia maszynowego, wykonanie biblioteki dll wykorzystującej wyniki działąnia ww. skryptów, wykonanie hosta", 1, "C#; Python", null, new DateTime(2021, 5, 26, 0, 0, 27, 706, DateTimeKind.Local).AddTicks(419), "Wtyczka VST realizująca proces masteringu poprzez autmoatyczne dopasowanie korektora EQ" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Classes",
+                keyColumn: "ClassId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Projects",
+                keyColumn: "ProjectId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Theses",
+                keyColumn: "ThesisId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Classes",
+                keyColumn: "ClassId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Teachers",
+                keyColumn: "UserID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Teachers",
+                keyColumn: "UserID",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "UserID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "UserID",
+                keyValue: 5);
+        }
+    }
+}
--- WD.Web/Models/Seeder.cs
+++ WD.Web/Models/Seeder.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System;
 using WD.Data.Models;
 using WD.Data.Tools;
 
@@ -35,6 +36,83 @@ namespace WD.Web.Models
                     Email = "matkas@student.agh.edu.pl",
                     Password = PasswordHasher.GetHashedPassword("matkas"),
                     HasThesis = true
+                });
+
+            modelBuilder.Entity<Teacher>().HasData(
+                new Teacher
+                {
+                    UserID = 4,
+                    Name = "Paweł",
+                    Surname = "Rotter",
+                    CanBePromoter = true,
+                    CanBeReviewer = true,
+                    Email = "rotter@agh.edu.pl",
+                    Password = PasswordHasher.GetHashedPassword("rotter")
+                },
+                new Teacher
+                {
+                    UserID = 5,
+                    Name = "Jerzy",
+                    Surname = "Baranowski",
+                    CanBePromoter = true,
+                    CanBeReviewer = true,
+                    Email = "baranowski@agh.edu.pl",
+                    Password = PasswordHasher.GetHashedPassword("baranowski")
+                });
+
+            modelBuilder.Entity<Class>().HasData(
+                new Class
+                {
+                    ClassId = 1,
+                    Name = "Laboratorium Specjalizacyjne"
+                },
+                new Class
+                {
+                    ClassId = 2,
+                    Name = "Seminarium Dyplomowe"
+                });
+
+            modelBuilder.Entity<Project>().HasData(
+                new Project
+                {
+                    ProjectId = 1,
+                    Title = "Wirtualny Dziekanat",
+                    CreationDate = DateTime.Now,
+                    ClassId = 1,
+                    IsReviewed = false,
+                    IsSubmitted = false,
+                    SubmissionDate = null,
+                    ReviewDate = null,
+                    Review = null,
+                    Note = null,
+                    Scope = "Wykonanie aplikacji webowej obsługującej wirtualny dziekanat uczelni wyższej",
+                    Goal = "Działająca i przetestowana aplikacja z funkcjonalnościami: dodawania ocen, oddawania projektów, składania pracy dyplomowej"
+                });
+
+            modelBuilder.Entity<Thesis>().HasData(
+                new Thesis
+                {
+                    StudentId = 1,
+                    SubmissionDate = null,
+                    AcceptationDate = null,
+                    CreationDate = DateTime.Now,
+                    IsAccepted = false,
+                    IsReviewed = false,
+                    IsSubmitted = false,
+                    IsTaken = true,
+                    TakeDate = DateTime.Now,
+                    ReviewDate = null,
+                    Review = null,
+                    PromoterId = 4,
+                    ReviewerId = 5,
+                    PromoterNote = null,
+                    ReviewerNote = null,
+                    PromoterOpinion = null,
+                    Title = "Wtyczka VST realizująca proces masteringu poprzez autmoatyczne dopasowanie korektora EQ",
+                    Goal = "Wykonanie działającej  i gotowej do użycia wtyczki",
+                    Scope = "Stowrzenie skryptów uczenia maszynowego, wykonanie biblioteki dll wykorzystującej wyniki działąnia ww. skryptów, wykonanie hosta",
+                    StudentQualifications = "C#; Python",
+                    ThesisId = 1
                 });
         }
     }
