3f156accd61c72151bbb3392ca3bdd6906dde3ec
Jakub <jakub.jachow@gmail.com>
2021-06-02 18:15:07 +0200

    Admin - edit user

---
+++ WD.Web/ViewModels/EditUserViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WD.Web.ViewModels
+{
+    public class EditUserViewModel
+    {
+        public string Id { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        public List<string> Roles { get; set; }
+        public List<string> Claims { get; set; }
+
+        public EditUserViewModel()
+        {
+            Roles = new List<string>();
+            Claims = new List<string>();
+        }
+    }
+}
